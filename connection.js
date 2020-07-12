b5991f2a41a0d8417de1bc29fe1469593c6b10b2
jjung2945 <42636296+jjung2945@users.noreply.github.com>
2020-07-12 19:43:58 +0900

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+async function main(){
+const uri = "mongodb+srv://Jaeheuk:2945>@cluster0-b5ozg.gcp.mongodb.net/Info?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+   const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+ 
